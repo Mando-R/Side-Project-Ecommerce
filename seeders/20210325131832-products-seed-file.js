3c254313c68afd051a628e1cc2daebed27e85485
Ryan Lai <ryanx94@gmail.com>
2021-03-26 03:14:57 +0800

    admin: Add seeder: category

--- seeders/20210325131832-products-seed-file.js
+++ seeders/20210325131832-products-seed-file.js
@@ -8,19 +8,22 @@ module.exports = {
     // bulkInsert()：Insert records into a table.
     // 用陣列形式，將 Restaurant 物件傳入 MySQL Database。
     await queryInterface.bulkInsert("Products",
-      // Array.from({ length: 50 })：建立長度為 50 的陣列
-      Array.from({ length: 50 })
-        // map(item, index, array)：讓長度為 50 的陣列，其中每一元素皆對應一個 "product 物件{Data}"，並回傳一個新 Array[]。
+      // Array.from({ length: 200 })：建立長度為 200 的陣列
+      Array.from({ length: 200 })
+        // map(item, index, array)：讓長度為 200 的陣列，其中每一元素皆對應一個 "product 物件{Data}"，並回傳一個新 Array[]。
         .map((item, index, array) => (
           {
             // faker：產生 fake Data，如 name、tel、address、description。
             name: faker.name.findName(),
             price: faker.commerce.price(),
             description: faker.lorem.text(),
+
             image: `https://loremflickr.com/320/240/electricvehicle?random=${Math.random() * 100}`,
             quantity: Math.floor(Math.random() * 11) * 10,
+
             // 對應 Category Seeder
-            CategoryId: Math.floor(Math.random() * 6) * 10 + 1,
+            CategoryId: Math.floor(Math.random() * 7) * 10 + 1,
+
             createdAt: new Date(),
             updatedAt: new Date(),
           }
